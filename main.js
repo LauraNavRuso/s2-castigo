ac99ac2e8a2a132ff5beff4c48fe02b2248dfb11
LauraNavRuso <laura.nav.ruso@gmail.com>
2017-12-12 09:12:23 +0100

    add for loop

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-'use strict'; 
+'use strict';
+
+var castigoElement = document.querySelector('.castigo');
+var phrase = '';
+
+for (var i=0; i<100; i++) {
+	phrase+= 'He aprendido bien cómo funcionan los bucles';
+	console.log(phrase);
+
+
+
+}
+castigoElement.innerHTML = phrase;

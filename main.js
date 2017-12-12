4898ed99919bade54ed984b398f798d7888fded3
Celia19C <thatcyclone@gmail.com>
2017-12-12 10:18:59 +0100

    Terminado

--- main.js
+++ main.js
@@ -2,12 +2,19 @@
 
 var castigoElement = document.querySelector('.castigo');
 var phrase = '';
+var colores= ['Blanco', 'Azul', 'Rojo', 'Verde', 'Amarillo', 'Rosa'];
+var color='';
+//
 
-for (var i=0; i<100; i++) {
-	phrase+= 'He aprendido bien cómo funcionan los bucles <br>';
+for (var z=0; z<colores.length; z++) {
+	color+= '<option>'+ colores[z] +'</option>';
 	console.log(phrase);
-
-
-
 }
+for (var i=0; i<100; i++) {
+	phrase+= 'He aprendido bien cómo funcionan los bucles <select>';
+
+	phrase+= color+ '</select> <br>';
+
+	}
+
 castigoElement.innerHTML = phrase;
